d275323e0c08eb502fc0257d3171788744bc26d1
Shikha Lodhi <77823971+shikha2961@users.noreply.github.com>
2022-06-04 18:41:50 +0530

    Create reverseLinkedList.cpp

---
+++ Day8/reverseLinkedList.cpp
@@ -0,0 +1,29 @@
+// Problem: https://leetcode.com/problems/reverse-linked-list/
+/*
+
+Approach: To reverse a linked list, we have to just reverse the sign of directions in linked list
+and Instead, NULL points to the last node, it should points to the first node.
+  1. Create a dummy node, Initialize it with NULL and a Next node points to the just next to the head.
+  2. While head is not equal to NULL, 
+      connect next of head to the dummy
+      Change now initialize dummy with head and head with next to change the next direction.
+        
+*/
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* dummy = NULL;
+        while(head!=NULL){
+            ListNode* Next = head->next;
+            head->next = dummy;
+            dummy = head;
+            head = Next;
+        }
+        return dummy;
+    }
+};
+
+/*
+Time Complexity: O(N)
+Space Complexity: O(1)
+*/
